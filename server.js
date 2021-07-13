5aae0915884691aac26025cf14d0cf501a92bc2f
Dhruvik <barvaliyadhruvik7@gmail.com>
2021-07-13 19:45:56 +0530

    /token api not working

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post("/login", (req, res) => {
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split('')[1]
+    const token = authHeader && authHeader.split(' ')[1]
     if (token == null) {
         return res.sendStatus(401)
     }
